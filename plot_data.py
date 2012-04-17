945b40cc4be200f86e0d7a3b328ddc64a2461189
Alex Lavigne-Gagnon <gagnon.alex@gmail.com>
2012-04-17 13:39:56 -0400

    turned plot_data.py into an actual function that can be called

--- plot_data.py
+++ plot_data.py
@@ -2,18 +2,13 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
-columns = test_load()
+def plot_data(columns):
+	columns = test_load()
+	plt.plot(columns)
+	plt.show()
 
-plt.plot(columns)
-
-# Creates a plot. Json data needs to be transposed.
-
-plt.xlabel('some text')
-
-plt.ylabel('some text')
-
-plt.title('Title')
-
-plt.show()
-
-# Actually displays the figure created by plt.plot
+# plt.plot('yourdata') plots your data, plt.show() displays the figure.
+# Json data needs to be transposed.
+# plt.xlabel('some text') = adds label on x axis
+# plt.ylabel('some text') = adds label on y axis
+# plt.title('Title') = adds title
